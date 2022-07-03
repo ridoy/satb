8871f4759e411c36f91ccb67557f484dc040a14a
ridoy <majumdar.ridoy@gmail.com>
2022-07-03 08:19:46 -0400

    Add mobile friendly

--- public/main.js
+++ public/main.js
@@ -7,12 +7,6 @@
  * Load page. Goes to submit. "Show she's a {num dropdown} but... " and textarea. Submit. Write to DB.
  *
  */
-
-/*
- * table prompts
- * rows... prompts
- * columns: text, 10 columns for rating counts, "he" or "she" column
- */
 console.log("Script loaded");
 
 
@@ -22,44 +16,59 @@ console.log("Script loaded");
 // show graphs
 
 
-const buttons = document.querySelectorAll(".preference-button");
+const prefButtons = document.querySelectorAll(".preference-button");
 const welcomeMenu = document.querySelector("#welcome-menu");
 const voteMenu = document.querySelector("#vote-menu");
 const scale = document.querySelector("#scale");
-const voteButton = document.querySelector(".vote-button");
+const voteButtons = document.querySelector(".vote-button");
 const header = document.querySelector(".header > h1");
 const prompt = document.querySelector("#prompt > h1");
-let preference = "she";
-buttons.forEach((button) => {
-    button.addEventListener("click", function(e) {
-        preference = e.target.value;
+
+let preference = null;
+
+prefButtons.forEach((prefButton) => {
+    prefButton.addEventListener("click", function(e) {
+        preference = e.target.value || "both";
         updateHeader();
         // Load example from DB
         // Clear page
         welcomeMenu.remove();
         // Populate page
         voteMenu.style.display = "block";
+    })
+});
+
+voteButtons.forEach((voteButton) => {
+    voteButton.addEventListener("click", (e) => {
+        let promptId = currentPromptId;
+        let selectedRating = e.target.value;
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                let data = JSON.parse(xhttp.responseText)[0];
-                let text = data.text; 
-                updateHeader(data.pref, data.defaultrating);
-                prompt.innerText = text;
+                // Confirmation message, go to next
+                
             }
         };
-        xhttp.open("GET", `/prompt?pref=${preference}`, true);
+        xhttp.open("GET", `/vote?promptId=${promotId}&rating=${selectedRating}`, true);
         xhttp.send();
-    })
+    });
 });
+
 
 function updateHeader(gender, defaultRating) {
     header.innerText = `${gender}'s a ${defaultRating} but...`;
 }
 
-voteButton.addEventListener("click", function(e) {
-    // get selected rating
-    // update db
-    // show results
-});
-
+function loadPrompt() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(xhttp.responseText)[0];
+            let text = data.text; 
+            updateHeader(data.pref, data.defaultrating);
+            prompt.innerText = text;
+        }
+    };
+    xhttp.open("GET", `/prompt?pref=${preference}`, true);
+    xhttp.send();
+}
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 'use strict';
+/*
+ * SATB Server
+ *
+ * v1 Functionality:
+ * Prompt: fetch a prompt from DB
+ * Vote: increment counter for a rating for a prompt in the DB
+ * Submit: store user-submitted prompts in a separate table for evaluation
+ */ 
 require('dotenv').config()
 
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
-
 const pg = require('pg');
 const pgClient = new pg.Client({
     connectionString: process.env.DATABASE_URL,
@@ -13,26 +19,10 @@ const pgClient = new pg.Client({
         rejectUnauthorized: false
     }
 });
-const prefix = "!";
+const port = process.env.PORT;
 
 pgClient.connect();
-
-
 app.use(express.static('public'));
-
-
-/*
-`SELECT * from prompts LIMIT 1`;
-
-//let query = `INSERT INTO leader (name, discordid, serverid, idhash) VALUES ($1, $2, $3, $4) ON CONFLICT (idhash) DO UPDATE SET count = leader.count + 1;`
-//let values = [name, discordid, serverid, idhash];
-console.log(`${name} got a react`);
-pgClient.query(query, values, (err, res) => {
-    if (err) throw err;
-});
-*/
-
-
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -44,7 +34,15 @@ app.get('/prompt', function(req, res) {
         if (err) throw err;
         res.send(data.rows);
     });
-})
+});
+
+app.get('/vote', function(req, res) {
+    let query = `SELECT * from prompts LIMIT 1`;
+    pgClient.query(query, (err, data) => {
+        if (err) throw err;
+        res.send(data.rows);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
